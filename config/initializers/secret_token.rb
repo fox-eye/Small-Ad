38a215bcba1ccf4366d384f38bf7b165c1873447
Lucas Romano <romano.lucas@gmail.com>
2011-09-07 02:09:37 +0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SmallAd::Application.config.secret_token = 'd8351fb1b9937e22837170bd4b5c5581cbd6db51a81fc6b96152173c96a58caa7c1d6c79fcd5a57ad2f78587f335bf7a291a5bf2b5e9ac12f4e1f5e3c0046611'
