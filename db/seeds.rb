340a42f71a17ee8fd960c85b44f2347c509045df
Lucas Romano <romano.lucas@gmail.com>
2011-09-17 18:03:32 +0200

    modified seed file to add some users and ads

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,21 @@
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
 
+require 'faker'
+
+def random_in_range(min,max)
+  min = min.to_i
+  max = max.to_i
+  arr = (min..max).to_a;
+  arr.at(rand(arr.length))
+end
+
+def rand_sub_cat
+  pos = rand(hash.size)
+  key = hash.keys[pos]
+  hash[key]
+end
+
 #add categories for ads
 categories = {
               "musical instruments" => ["string instruments","electric string instruments","wind instrument","percussion instrument"],
@@ -20,5 +35,38 @@ categories.each do |category_name,subcat|
   category = Category.create(:name => category_name)
   subcat.each do |subcat_name|
     category.children.create(:name => subcat_name)
+  end
+end
+
+# ad some users and their ads
+15.times do
+   
+  # créer un nouvel utilisateur
+  user = User.create({:first_name => Faker::Name::first_name,
+               :last_name => Faker::Name::last_name,
+               :email => Faker::Internet::email,
+               :city => Faker::Address::city,
+               :password => "test123",
+               :password_confirmation => "test123"
+              })
+              
+  # Générer 5 nouvelles annonces dans des sous catégories en random pour cet utilisateur
+  5.times do
+    #choisir une sous categorie au hasard dans une catégorie choisie au hasard
+    subcategories = rand_sub_cat(categories)
+    subcategory_name = categories[rand(categories.size)]
+    
+    # trouver l'id de la sous catégorie
+    subcategory_id = Category.find_by_name(subcategory_name)
+    
+    ad = user.ads.build({
+                    :title => Faker::Lorem.words(random_in_range(4,10)),
+                    :description => Faker::Lorem.sentences(random_in_range(1,4)),
+                    :price => random_in_range(10,250)
+                  })
+                  
+    ad.category_id = subcategory_id
+    
+    ad.save
   end
 end
